2c8a9d9de17030d6277916a4ef1ca11030db6378
pedrosale <53319245+pedrosale@users.noreply.github.com>
2024-02-09 12:46:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,8 @@ def main():
     initialize_session_state()
     st.title('Este é um modelo de papagaio estocástico para debater sobre como lidar com eles (ou pelo menos tentar - uma vez que a própria conceituação do tema permanece imprecisa...).')
     st.image("https://raw.githubusercontent.com/pedrosale/papagaio_estocastico/9b270efddaae8743aeec4f5f9cff7327a0e73dee/p_est.jpeg", caption="Papagaio Estocástico", width=225)
-    st.markdown('**Esta versão contém:**  \nA) Modelo llama2 com refinamento de parâmetros;  \nB) Ajuste de prompt para debate sobre Alucinação do modelo;  \nC) Conjuntos de dados pré-carregados referente ao tema [Veja os dados](https://raw.githubusercontent.com/pedrosale/papagaio_estocastico/main/AI-Hallucinations-A-Misnomer-Worth-Clarifying.txt);  \nD) Processamento dos dados carregados com uso da biblioteca Langchain.')
+    st.markdown('**Esta versão contém:**  \nUm conjuntos de dados pré-carregados referente ao tema [Veja os dados](https://raw.githubusercontent.com/pedrosale/papagaio_estocastico/main/AI-Hallucinations-A-Misnomer-Worth-Clarifying.txt)'.
+                
     # Carrega o arquivo diretamente (substitua o caminho do arquivo conforme necessário)
 
 
