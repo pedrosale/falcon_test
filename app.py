5691ff0ed051444a75d5ef7aac9f2812919eafa5
pedrosale <53319245+pedrosale@users.noreply.github.com>
2024-02-16 11:09:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import streamlit as st
-from streamlit_chat import message
-from langchain.chains import ConversationalRetrievalChain
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import CTransformers
-from langchain.llms import Replicate
-from langchain.text_splitter import CharacterTextSplitter  # Importe esta linha
-from langchain.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import Docx2txtLoader
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain import HuggingFaceHub
+from apikey_hungingface import apikey_hungingface
+from langchain import PromptTemplate, LLMChain
 import os
-from dotenv import load_dotenv
-import tempfile
-import urllib.request
-import requests
 
-load_dotenv()
+# Set Hugging Face Hub API token
+# Certifique-se de armazenar seu token API no arquivo `apikey_hungingface.py`
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = apikey_hungingface
 
+# Set up the language model using the Hugging Face Hub repository
+repo_id = "tiiuae/falcon-7b-instruct"
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.3, "max_new_tokens": 2000})
+
+# Set up the prompt template
+template = """
+You are an artificial intelligence assistant.
+The assistant gives helpful, detailed, and polite answers to the user's question
+Question: {question}\n\nAnswer: Let's think step by step."""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 def initialize_session_state():
     if 'history' not in st.session_state:
@@ -28,19 +28,12 @@ def initialize_session_state():
         st.session_state['generated'] = ["Como posso te ajudar?"]
 
     if 'past' not in st.session_state:
-        st.session_state['past'] = ["Olá, sou modelo que deve responder!"]
+        st.session_state['past'] = ["Olá, sou assistente do Pedro."]
 
 def conversation_chat(query, chain, history):
-    prompt = """
-    Você é um assistente que possui as duas funções a seguir:
-    1. Você é um assistente que só conversa no idioma português do Brasil (você nunca, jamais conversa em outro idioma que não seja o português do Brasil). 
-    2. Você é um assistente que possui como objetivo falar coisas aleatórias a respeito de alucinação de modelos. 
-    """  # Adicionando prompt para indicar o idioma
-    query_with_prompt = prompt + query
-    result = chain({"question": query_with_prompt, "chat_history": history})
-    history.append((query, result["answer"]))
-    return result["answer"]
-
+    result = chain.run(query)
+    history.append((query, result))
+    return result
 
 def display_chat_history(chain):
     reply_container = st.container()
@@ -48,7 +41,7 @@ def display_chat_history(chain):
 
     with container:
         with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_input("question:", placeholder="Me pergunte sobre os conjuntos de dados pré-carregados", key='input')
+            user_input = st.text_input("question:", placeholder="Me pergunte sobre o(s) conjunto(s) de dados pré-carregados", key='input')
             submit_button = st.form_submit_button(label='Enviar')
 
         if submit_button and user_input:
@@ -61,86 +54,17 @@ def display_chat_history(chain):
     if st.session_state['generated']:
         with reply_container:
             for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-                message(st.session_state["generated"][i], key=str(i))
-
-def create_conversational_chain(vector_store):
-    load_dotenv()
-
-    llm = Replicate(
-        streaming = True,
-        model = "replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781", 
-        #model = "tomasmcm/towerinstruct-7b-v0.1",         
-        language="pt-BR",  
-        callbacks=[StreamingStdOutCallbackHandler()],
-        input = {"temperature": 0.01, "max_length" :500,"top_p":1})
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
-                                                 retriever=vector_store.as_retriever(search_kwargs={"k": 2}),
-                                                 memory=memory)
-    return chain
-
+                st.text_input("Pergunta:", value=st.session_state["past"][i], key=str(i) + '_user', disabled=True)
+                st.text_input("Resposta:", value=st.session_state["generated"][i], key=str(i), disabled=True)
 
 def main():
-    load_dotenv()
-# Initialize session state
     initialize_session_state()
-    st.title('Que tal conversarmos sobre imprecisão das respostas geradas pelos modelos de Large Language Models ?')
-    st.markdown('**Esta versão contém:**')
-   
+    st.title('[Versão 3.0] 🦙💬 FALCON Chatbot desenvolvido por Pedro Sampaio Amorim.')
+    image_url = 'https://raw.githubusercontent.com/pedrosale/bot2/168f145c9833dcefac6ccab4c351234e819a5e97/fluxo%20atual.jpg'
+    st.image(image_url, caption='Arquitetura atual: GitHub + Streamlit')
+    st.markdown('**Esta versão contém:**  \nA) Modelo FALCON;  \nB) Conjunto de dados pré-carregados do CTB [1. Arquivo de Contexto](https://raw.githubusercontent.com/pedrosale/bot2/main/CTB3.txt) e [2. Reforço de Contexto](https://raw.githubusercontent.com/pedrosale/bot2/main/CTB2.txt);  \nC) Processamento dos dados carregados (em B.) com uso da biblioteca Langchain.')
 
-
-
-    # Carrega o primeiro arquivo diretamente
-    file_path1 = "https://raw.githubusercontent.com/pedrosale/papagaio_estocastico/main/AI-Hallucinations-A-Misnomer-Worth-Clarifying.txt"
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file1:
-        temp_file1.write(urllib.request.urlopen(file_path1).read())
-        temp_file_path1 = temp_file1.name
-
-    text1 = []
-    loader1 = TextLoader(temp_file_path1)
-    text1.extend(loader1.load())
-    os.remove(temp_file_path1)
-    
-    # Carrega o segundo arquivo diretamente
-    file_path2 = "https://raw.githubusercontent.com/pedrosale/papagaio_estocastico/main/What%20are%20AI%20hallucinations_%20_%20IBM.txt"
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file2:
-        temp_file2.write(urllib.request.urlopen(file_path2).read())
-        temp_file_path2 = temp_file2.name
-
-    text2 = []
-    loader2 = TextLoader(temp_file_path2)
-    text2.extend(loader2.load())
-    os.remove(temp_file_path2)
-
-    # Carrega o segundo arquivo diretamente
-
-    file_path3 = "https://raw.githubusercontent.com/pedrosale/papagaio_estocastico/main/A%20Survey%20on%20Hallucination%20in%20Large%20Language%20Models.pdf"
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file3:
-        temp_file3.write(urllib.request.urlopen(file_path3).read())
-        temp_file_path3 = temp_file3.name
-
-    text3 = PyPDFLoader(temp_file_path3).load()
-    os.remove(temp_file_path3)
-        
-    # Combina os textos carregados dos dois arquivos
-    text = text1 + text2 + text3
-
-    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=100, length_function=len)
-    text_chunks = text_splitter.split_documents(text)
-
-    # Create embeddings
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                      model_kwargs={'device': 'cpu'})
-
-    # Create vector store
-    vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
-
-    # Create the chain object
-    chain = create_conversational_chain(vector_store)
-
-    display_chat_history(chain)
+    display_chat_history(llm_chain)
 
 if __name__ == "__main__":
     main()
